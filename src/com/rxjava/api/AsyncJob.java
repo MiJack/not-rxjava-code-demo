4014a64f1ceef5b95c8836d456dd58dc2bf505dd
MiJack <893380824@qq.com>
2015-12-25 20:11:34 +0800

    添加FlatMap

--- src/com/rxjava/api/AsyncJob.java
+++ src/com/rxjava/api/AsyncJob.java
@@ -26,4 +26,35 @@ public abstract class AsyncJob<T> {
             }
         };
     }
+
+    public <R> AsyncJob<R> flatMap(Func<T, AsyncJob<R>> func) {
+        final AsyncJob<T> source = this;
+        return new AsyncJob<R>() {
+            @Override
+            public void start(Callback<R> callback) {
+                source.start(new Callback<T>() {
+                    @Override
+                    public void onResult(T result) {
+                        AsyncJob<R> mapped = func.call(result);
+                        mapped.start(new Callback<R>() {
+                            @Override
+                            public void onResult(R result) {
+                                callback.onResult(result);
+                            }
+
+                            @Override
+                            public void onError(Exception e) {
+                                callback.onError(e);
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onError(Exception e) {
+                        callback.onError(e);
+                    }
+                });
+            }
+        };
+    }
 }
