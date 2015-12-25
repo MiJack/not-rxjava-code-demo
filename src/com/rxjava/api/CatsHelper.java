0463df83edd9e0d01a89527fb4510c1a5dab010e
MiJack <893380824@qq.com>
2015-12-25 20:04:41 +0800

    使用 AsyncJobs 把我们的方法分解成更小的操作

--- src/com/rxjava/api/CatsHelper.java
+++ src/com/rxjava/api/CatsHelper.java
@@ -14,35 +14,51 @@ public class CatsHelper {
     ApiWrapper apiWrapper;
 
     public AsyncJob<Uri> saveTheCutestCat(String query) {
-        return new AsyncJob<Uri>() {
+        AsyncJob<List<Cat>> catsListAsyncJob = apiWrapper.queryCats(query);
+        AsyncJob<Cat> cutestCatAsyncJob = new AsyncJob<Cat>() {
             @Override
-            public void start(Callback<Uri> cutestCatCallback) {
-                apiWrapper.queryCats(query)
-                        .start(new Callback<List<Cat>>() {
-                            @Override
-                            public void onResult(List<Cat> cats) {
-                                Cat cutest = findCutest(cats);
-                                apiWrapper.store(cutest)
-                                        .start(new Callback<Uri>() {
-                                            @Override
-                                            public void onResult(Uri result) {
-                                                cutestCatCallback.onResult(result);
-                                            }
+            public void start(Callback<Cat> callback) {
+                catsListAsyncJob.start(new Callback<List<Cat>>() {
+                    @Override
+                    public void onResult(List<Cat> result) {
+                        callback.onResult(findCutest(result));
+                    }
 
-                                            @Override
-                                            public void onError(Exception e) {
-                                                cutestCatCallback.onError(e);
-                                            }
-                                        });
-                            }
-
-                            @Override
-                            public void onError(Exception e) {
-                                cutestCatCallback.onError(e);
-                            }
-                        });
+                    @Override
+                    public void onError(Exception e) {
+                        callback.onError(e);
+                    }
+                });
             }
         };
+        AsyncJob<Uri> storedUriAsyncJob = new AsyncJob<Uri>() {
+            @Override
+            public void start(Callback<Uri> cutestCatCallback) {
+                cutestCatAsyncJob.start(new Callback<Cat>() {
+                    @Override
+                    public void onResult(Cat cutest) {
+                        apiWrapper.store(cutest)
+                                .start(new Callback<Uri>() {
+                                    @Override
+                                    public void onResult(Uri result) {
+                                        cutestCatCallback.onResult(result);
+                                    }
+
+                                    @Override
+                                    public void onError(Exception e) {
+                                        cutestCatCallback.onError(e);
+                                    }
+                                });
+                    }
+
+                    @Override
+                    public void onError(Exception e) {
+                        cutestCatCallback.onError(e);
+                    }
+                });
+            }
+        };
+        return storedUriAsyncJob;
     }
 
     private Cat findCutest(List<Cat> cats) {
